14398347713577bdff28a484d858100d3111b1c6
Vaibhav Jha <vaibhavjha100@gmail.com>
2025-08-13 17:36:39 +1000

    Update date range in Economic Times example script

--- examples/economic_times_example.py
+++ examples/economic_times_example.py
@@ -3,6 +3,6 @@ from datetime import datetime, timedelta
 
 if __name__ == "__main__":
     # Choose a small date range to limit results
-    df = scrape_economic_times('2025-01-01', '2025-01-02')
+    df = scrape_economic_times('2025-02-10', '2025-02-11')
     print(df.head())
     print(df.info())
