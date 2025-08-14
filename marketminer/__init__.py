7180ae007bf0a29a46c0a39a02b1ee6af5102aca
Vaibhav Jha <vaibhavjha100@gmail.com>
2025-08-14 14:53:37 +1000

    Add news scraper functions to module exports

--- marketminer/__init__.py
+++ marketminer/__init__.py
@@ -4,3 +4,10 @@ MarketMiner: A Python library for scraping financial data from various sources.
 
 __version__ = '0.1.0'
 
+from .news_scraper import scrape_economic_times
+from .utils import date_to_excel_serial
+
+__all__ = [
+    'scrape_economic_times',
+    'date_to_excel_serial'
+]
