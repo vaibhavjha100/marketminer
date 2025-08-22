6baea50b358332d4c3dcdb2396f36969a83fff89
Vaibhav Jha <vaibhavjha100@gmail.com>
2025-08-22 23:44:19 +1000

    Bump version to 0.6.0

--- marketminer/__init__.py
+++ marketminer/__init__.py
@@ -2,7 +2,7 @@
 MarketMiner: A Python library for scraping financial data from various sources.
 '''
 
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 
 from .news_scraper import scrape_economic_times
 from .fundamentals_scraper import scrape_fundamentals
